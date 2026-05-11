cdff3bd09040da0420b3cf1e70595f856cbd0a1a
srilatha2012 <srilathp2012@gmail.com>
2026-05-11 19:15:13 -0400

    Built the express server and imported required libraries and created router handler for index.html and contact.html

---
+++ server.js
@@ -0,0 +1,35 @@
+//import the express library
+const express = require("express");
+
+//import built-in path module
+//path helps us safely create file paths
+const path = require("path");
+
+//create an express application
+const app = express();
+
+//define the port number
+const PORT = 3002;
+
+//serve static files from public folder
+app.use(express.static(path.join(__dirname,"public")));
+
+
+//route for the home page
+app.get("/", (request,response) => {
+ response.sendFile(path.join(__dirname,"public","index.html"));
+
+});
+
+//route for the contact page
+app.get("/contact", (request,response) => {
+   response.sendFile(path.join(__dirname,"public","contact.html"));
+});
+
+//Start the server
+app.listen(PORT, ()=>{
+    console.log(`Running on port ${PORT}`)
+})
+
+
+
